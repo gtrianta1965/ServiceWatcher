b2f492ade6999d44d70f8d3606c3152b14e12952
Akorilli <Akorilli@ORA-linux>
2017-07-03 13:09:26 +0300

    DBservice Changes Final

--- ServiceWatcher/src/main/java/com/cons/services/DBService.java
+++ ServiceWatcher/src/main/java/com/cons/services/DBService.java
@@ -4,12 +4,33 @@ import com.cons.services.ServiceParameter;
 
 import com.cons.test.DBServiceExample;
 
+import com.cons.utils.SWConstants;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Driver;
 
 public class DBService extends Service {
+    private static String userName;
+    private static String password;
+
+    public static void setUserName(String userName) {
+        DBService.userName = userName;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setPassword(String password) {
+        DBService.password = password;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
     public DBService() {
     }
 
@@ -31,10 +52,10 @@ public class DBService extends Service {
 
                 DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
                 Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
-                conn = DriverManager.getConnection(currentUrl);
+                conn = DriverManager.getConnection(currentUrl,userName,password);
                 
             } catch (ClassNotFoundException ex) {
-                this.setErrorCall(ex.getMessage());
+                this.setErrorCall(SWConstants.SERVICE_DB_ERROR_ORACLE_CLASS_MSG+ex.getMessage());
                 this.setSuccessfulCall(false);
             } catch (IllegalAccessException ex) {
                 this.setErrorCall(ex.getMessage());
@@ -43,7 +64,7 @@ public class DBService extends Service {
                 this.setSuccessfulCall(false);
                 this.setErrorCall(ex.getMessage());
             } catch (SQLException ex) {
-                this.setErrorCall(ex.getMessage());
+                this.setErrorCall(SWConstants.SERVICE_DB_ERROR_ORACLE_SQLEXCEPTION_MSG+ex.getMessage());
                 this.setSuccessfulCall(false);
             }
 

01ec577124896bdfab354e65a4280bf7c9936771
Magdalini Varvouzou <magdoula29v@hotmail.com>
2017-07-19 11:26:27 +0300

    -Correct the Jtable Dimentions and Change the title Color

--- ServiceWatcher/src/main/java/com/cons/ui/MainFrame.java
+++ ServiceWatcher/src/main/java/com/cons/ui/MainFrame.java
@@ -220,6 +220,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         lblVersion.setBackground(javax.swing.UIManager.getDefaults().getColor("CheckBox.focus"));
         lblVersion.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        lblVersion.setForeground(new java.awt.Color(102, 102, 255));
         lblVersion.setText("Service Watcher v1.0");
 
         checkAutoRefresh.setText("Auto-Refresh");
@@ -243,7 +244,7 @@ public class MainFrame extends javax.swing.JFrame {
         statusRun.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         statusRun.setIcon(this.getImg());
         statusRun.setText(getServiceOrchestrator()!=null?(!serviceOrchestrator.isRunning()?"IDLE":""):"IDLE");
-        statusRun.setAlignmentX(0.5F);
+        statusRun.setAlignmentX((float) 0.5);
 
         javax.swing.GroupLayout statusBarSection2Layout = new javax.swing.GroupLayout(statusBarSection2);
         statusBarSection2.setLayout(statusBarSection2Layout);
@@ -327,17 +328,15 @@ public class MainFrame extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1018, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addComponent(jScrollPane1)
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 291, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 303, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -350,9 +349,9 @@ public class MainFrame extends javax.swing.JFrame {
                     .addComponent(lblVersion, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(checkAutoRefresh)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(cbAutoRefreshInterval, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 506, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 500, Short.MAX_VALUE)
                         .addComponent(btnLoad, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnRefresh, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)

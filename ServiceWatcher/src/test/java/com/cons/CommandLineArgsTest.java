91fd74ceb316e9cd01a7b9c22298d368783c6e8a
agregori <androklis.gregoriou@oracle.com>
2017-07-30 13:03:58 +0300

    CommandLineArgsTest changed setCommandLineArgs method to init

--- ServiceWatcher/src/test/java/com/cons/CommandLineArgsTest.java
+++ ServiceWatcher/src/test/java/com/cons/CommandLineArgsTest.java
@@ -25,7 +25,7 @@ public class CommandLineArgsTest {
     @Test
     public void testEmptyArgs() {
         String[] args = { };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals("config.properties", cla.getConfigFile());
         Assert.assertEquals(false, cla.getEncrypt());
         Assert.assertEquals(false, cla.isNoGUI());
@@ -35,64 +35,64 @@ public class CommandLineArgsTest {
     @Test
     public void testValidConfArg() {
         String[] args = { "-conf:test.properties" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals("test.properties", cla.getConfigFile());
     }
 
     @Test
     public void testInvalidConfArg() {
         String[] args = { "-conf/home/agregori/test.properties" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals("config.properties", cla.getConfigFile());
     }
 
     @Test
     public void testValidEncryptArg() {
         String[] args = { "-encrypt" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals(true, cla.getEncrypt());
     }
 
     @Test
     public void testInvalidEncryptArg() {
         String[] args = { "-encrypt:" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals(false, cla.getEncrypt());
     }
 
     @Test
     public void testValidNoGUIArg() {
         String[] args = { "-nogui" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals(true, cla.isNoGUI());
     }
 
     @Test
     public void testInvalidNoGUIArg() {
         String[] args = { "-nogui:" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals(false, cla.isNoGUI());
     }
 
     @Test
     public void testValidDefaultAutoRefreshTimeArg() {
         String[] args = { "-autorefresh" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals(1, cla.getAutoRefreshTime());
     }
 
     @Test
     public void testValidAutoRefreshTimeArg() {
         String[] args = { "-autorefresh:4" };
-        cla.setCommandLineArgs(args);
+        cla.init(args);
         Assert.assertEquals(4, cla.getAutoRefreshTime());
     }
 
     @Test
     public void testInvalidAutoRefreshTimeArg() {
         String[] args = { "-autorefresh4" };
-        cla.setCommandLineArgs(args);
-        Assert.assertEquals(0, cla.getAutoRefreshTime());
+        cla.init(args);
+        Assert.assertEquals(1, cla.getAutoRefreshTime());
     }
 
 }

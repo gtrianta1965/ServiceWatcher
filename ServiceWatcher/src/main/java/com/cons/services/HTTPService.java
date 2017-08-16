4c19b8b75e5bea135e9792450ef9366442509471
Magda Varvouzou <magdoula29v@hotmail.com>
2017-08-16 11:39:41 +0300

    http logger

--- ServiceWatcher/src/main/java/com/cons/services/HTTPService.java
+++ ServiceWatcher/src/main/java/com/cons/services/HTTPService.java
@@ -1,8 +1,6 @@
 package com.cons.services;
 
 
-import com.cons.Configuration;
-
 import com.cons.utils.SWConstants;
 
 import java.io.BufferedReader;
@@ -11,12 +9,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
-
 import java.net.URL;
 
-import java.util.List;
+import org.apache.log4j.Logger;
 
 public class HTTPService extends Service {
+    final static Logger logger = Logger.getLogger(HTTPService.class);
     public HTTPService() {
         super();
     }
@@ -33,13 +31,16 @@ public class HTTPService extends Service {
         String responseMessage = null;
         boolean found = false;
         try {
+            logger.info("starting an HTTP connection");
             URL url = new URL(serviceParameter.getUrl());
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
             huc.setRequestMethod("GET");
             if (configuration != null) {
                 huc.setConnectTimeout(configuration.getHttpResponseTimeout());
+                logger.debug("connection timed out");
             }
             huc.connect();
+            logger.debug("connected");
             responseCode = huc.getResponseCode();
             responseMessage = huc.getResponseMessage();
 
@@ -50,35 +51,43 @@ public class HTTPService extends Service {
                 while ((inputLine = in.readLine()) != null) {
                     if (serviceParameter.getSearchString() != null) {
                         if (inputLine.toLowerCase().contains(serviceParameter.getSearchString().toLowerCase())) {
+                            logger.info("connection success");
                             this.setSuccessfulCall(true);
                             found = true;
                             break;
                         }
                     } else {
+                        logger.info("connection success");
                         this.setSuccessfulCall(true);
                         break;
                     }
 
                 }
                 if (serviceParameter.getSearchString() != null && found == false) { //if no match found for getSearchString
+                    logger.info("connection failed");
                     this.setSuccessfulCall(false);
                     this.setErrorCall(SWConstants.SEARCH_STRING_NOT_FOUND_MSG);
                 }
             } else { //if status is <>200
+                logger.error(SWConstants.URL_RESPONSE_ERROR_MSG + responseMessage);
                 this.setSuccessfulCall(false);
                 this.setErrorCall(SWConstants.URL_RESPONSE_ERROR_MSG + responseMessage);
             }
-
+            
+            logger.debug("connection closed");
             in.close();
 
         } catch (MalformedURLException e) {
             this.setSuccessfulCall(false);
+            logger.error(SWConstants.MALLFORMED_URL_EXCEPTION_MSG + e.getMessage());
             this.setErrorCall(SWConstants.MALLFORMED_URL_EXCEPTION_MSG + e.getMessage());
         } catch (ProtocolException e) {
             this.setSuccessfulCall(false);
+            logger.error(SWConstants.PROTOCOL_EXCEPTION_MSG + e.getMessage());
             this.setErrorCall(SWConstants.PROTOCOL_EXCEPTION_MSG + e.getMessage());
         } catch (Exception e) {
             this.setSuccessfulCall(false);
+            logger.error(SWConstants.GENERIC_EXCEPTION_MSG + e.getMessage());
             this.setErrorCall(SWConstants.GENERIC_EXCEPTION_MSG + e.getMessage());
         }
     }

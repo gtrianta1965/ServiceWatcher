53621e065ce1f45f8953270d2a71497b28891c14
Alexandros Kalavitis <alexkalavitis@gmail.com>
2017-08-25 13:08:06 +0300

    deploy input has to be '/path/to/folder/', path changed to <input>ServiceWatcher/. Example if './deploy.sh /home/oracle/Desktop/' creates a folder named ServiceWatcher in Desktop and deploys there

--- ServiceWatcher/deploy.sh
+++ ServiceWatcher/deploy.sh
@@ -2,17 +2,17 @@ ROOT_F=$1
 ./rebuild.sh
 printf "\n[\033[1;38;2;0;255;0mINFO\033[0m] Deploying at path $ROOT_F/\n"
 # Make dir structure
-mkdir "$ROOT_F"
-mkdir "$ROOT_F"/target/
-mkdir "$ROOT_F"/target/lib/
+mkdir "$ROOT_F"ServiceWatcher/
+mkdir "$ROOT_F"ServiceWatcher/target/
+mkdir "$ROOT_F"ServiceWatcher/target/lib/
 # Copy needed files for stand alone
-cp report_template.html "$ROOT_F"/
-cp target/lib/* "$ROOT_F"/target/lib/
-cp target/ServiceWatcher* "$ROOT_F"/target/
-cp sample-config.properties "$ROOT_F"/
+cp report_template.html "$ROOT_F"ServiceWatcher/
+cp target/lib/* "$ROOT_F"ServiceWatcher/target/lib/
+cp target/ServiceWatcher* "$ROOT_F"ServiceWatcher/target/
+cp sample-config.properties "$ROOT_F"ServiceWatcher/
 # Copy executable scripts for win/linux
-cp sw.bat "$ROOT_F"/
-cp appendSW.bat "$ROOT_F"/
-cp sw.sh "$ROOT_F"/
+cp sw.bat "$ROOT_F"ServiceWatcher/
+cp appendSW.bat "$ROOT_F"ServiceWatcher/
+cp sw.sh "$ROOT_F"ServiceWatcher/
 printf "[\033[1;38;2;0;255;0mINFO\033[0m] Application deployed at -> $ROOT_F\n"
-printf "[\033[1;38;2;0;255;0mINFO\033[0m] To run Service Watcher\n-> Linux run './sw.sh' in a Terminal\n-> Windows run 'sw.bat' in CMD\nLocated in "$ROOT_F"/bin/\n"
+printf "[\033[1;38;2;0;255;0mINFO\033[0m] To run Service Watcher\n-> Linux run './sw.sh' in a Terminal\n-> Windows run 'sw.bat' in CMD\nLocated in "$ROOT_F"ServiceWatcher/\n"

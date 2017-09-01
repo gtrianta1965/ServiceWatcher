8c7d0665e5a9b3075c13c5ffad97ad5749340154
Androklis Gregoriou <androklis.greg@gmail.com>
2017-09-01 10:28:43 +0300

    Added validateRegex method in GenericUtils.java

--- ServiceWatcher/src/main/java/com/cons/utils/GenericUtils.java
+++ ServiceWatcher/src/main/java/com/cons/utils/GenericUtils.java
@@ -31,13 +31,23 @@ public class GenericUtils {
     public static boolean isWindows() {
         String OS = System.getProperty("os.name").toLowerCase();
         return (OS.indexOf("win") >= 0);
-
     }
 
     public static boolean isUnix() {
         String OS = System.getProperty("os.name").toLowerCase();
         return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+    }
 
+    public static boolean validateRegex(String regex) {
+        try {
+            java.util
+                .regex
+                .Pattern
+                .compile(regex);
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
     }
 
 }
